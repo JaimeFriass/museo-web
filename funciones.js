cbd09d009db444d091b4340967ab49efa1133884
Jaime Frías <jaimefrias_76@hotmail.com>
2018-04-03 19:04:21 +0200

    Muchas cosas mas

--- funciones.js
+++ funciones.js
@@ -15,6 +15,7 @@ function cambiartabu(text1){
 function mostrarComentarios() {
 
   var e = document.getElementById("comentarios");
+  e.style.right = pos;
   e.hidden = false;
   var pos = -300; // Posicion right del div
   var id = setInterval(frame, 1); // Cada 1ms se llamara a la funcion FRAME
@@ -27,4 +28,32 @@ function mostrarComentarios() {
       e.style.right = pos + 'px';   // Se modifica el atributo left del elemento
     }
   }
+
+}
+
+function validarForm() {
+  var nombre = document.forms["comentar"]["nombre"].value;
+  var email = document.forms["comentar"]["e-mail"].value;
+  var comentario = document.forms["comentar"]["mensaje"].value;
+
+  if (nombre == '' || email == '' || comentario == '') {
+    alert("Algo esta vacío");
+  } else {
+    // Si esta correcto el comentario se envia
+    var fecha = new Date();
+    var dia = fecha.getDate(); var mes = fecha.getMonth();
+    var anio = fecha.getFullYear(); var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+
+    var fecha_form = dia + "/" + mes + "/" + anio + " " + hora + ":" + minutos;
+
+    var contenedor = document.getElementById("lista_comentarios");
+
+    mensaje = cambiartabu(mensaje);
+    contenedor.innerHTML += "<div class='mensaje'><p class='nombre_com'><b>"+nombre+"</b></p><p class='fecha_com' >"
+                            +fecha_form +"</p><p>"+comentario+"</p></div>";
+
+  }
+
+  return false; // Para que no se actualice la pagina actual
 }
