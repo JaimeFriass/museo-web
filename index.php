047e70b1571a7cea6306ab4fc40cb27ee55fe33e
Unknown <nazaret.dragonball@gmail.com>
2018-04-07 16:03:14 +0200

    Administrando Base de Datos

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 include "head.php";
 include "header.php";
 
+
+    $conexion = mysql_connect ("localhost", "root", ""); //establece conexion
+    $abreBD = mysql_select_db ("museoweb", $conexion); //abre la BD
+    
+    if (!$abreBD) { 
+        die('No  se  pudo  abrir  la  base  de  datos.Error:  '.mysql_error()); 
+    } 
+    
+
+    <input type=int name="id">;
+    $id_url = $_GET['id'] ;
+
+    if( id_url < 8 && id_url >= 0){ 
+        $seleccion = 'SELECT  *  FROM  Obras Where Obras.id == id_url' ; //sentencia en sql 
+        $resultado = mysql_query ($seleccion, $conexion);   //ejecuta la sentencia y devuelve un resultado
+        //Ahora tenemos que redirigir la página
+    }
+
 ?>
