9d04d97da40a682631f3a8695095983b8138c894
Jaime Frías <jaimefrias_76@hotmail.com>
2018-04-11 14:08:25 +0200

    Obras sacadas de BD correctamente

--- obra.php
+++ obra.php
@@ -1,8 +1,40 @@
 <?php
 // CONSULTAMOS LOS DATOS DE LA OBRA EN CUESTION
-// TODO: Conectar con la base de datos
-// TODO: Sacar por GET la obra en cuestion
-// TODO: Consultar la obra en la base de datos
+// TODO: Cambiar ROOT
+// TODO: Cambiar utf
+
+$conexion = mysqli_connect ("localhost", "root", ""); //establece conexion
+$abreBD = mysqli_select_db ($conexion, "museoweb"); //abre la BD
+
+if (mysqli_connect_errno()) {
+    echo "Falló la conexión: ".mysqli_connect_error();
+    exit();
+}
+$id_obra = $_GET["id"];
+$seleccion = "SELECT  *  FROM obras WHERE id = ".$id_obra ; //sentencia en sql 
+
+if ($resultado = mysqli_query ($conexion, $seleccion)) {   //ejecuta la sentencia y devuelve un resultado
+    $numero = $resultado->num_rows;
+    echo $numero;
+} else {
+    echo "Falló el query";
+    exit();
+}
+
+// Pasamos el resultado a un array para extraer el id
+$array_resultado = mysqli_fetch_assoc($resultado);
+
+
+// Extraemos del array las variables que utlizaremos
+$nombre      = $array_resultado["Nombre"];
+echo "NOMBRE: ".$nombre;
+$fecha       = $array_resultado['fecha'];
+$dimensiones = $array_resultado['dimensiones'];
+$procedencia = $array_resultado['procedencia'];
+$comentario  = $array_resultado['comentario'];
+$imagen      = $array_resultado['imagen'];
+
+mysqli_close($conexion);
 ?>
 
 <html>
@@ -10,7 +42,8 @@
     include "head.php";
 ?>
 <body>
-    <?php include "header.php";
+    
+    <?php $menu_activo = 2; include "header.php";
 
     // Aqui modificamos las variables de cada obra
     include "contenido_obra.php";
