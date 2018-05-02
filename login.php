a25ab97f353b77236b697d72489a68898c0a21fb
Unknown <nazaret.dragonball@gmail.com>
2018-05-02 14:31:29 +0200

    Login

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php include "header.php"; 
+    include "footer.php";?>
+
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <title>Login</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+
+<form action="login.php">
+  User:<br>
+  <input type="text" name="name" value="Name">
+  <br>
+  Pass:<br>
+  <input type="password" name="pass" value="Password">
+  <br><br>
+  <input type="submit" value="Login">
+</form> 
+
+
+
+</body>
+</html>
