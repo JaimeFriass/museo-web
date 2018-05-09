9263a71c6dc92c87a256439ec43f1b1ab7f46900
Unknown <nazaret.dragonball@gmail.com>
2018-05-09 14:09:50 +0200

    Añadida ususarios con tipo

--- museoweb.sql
+++ museoweb.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2018 a las 14:03:31
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.6.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `museoweb`
 --
@@ -152,15 +134,16 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(50) NOT NULL,
   `id` int(11) NOT NULL,
   `correo` varchar(50) DEFAULT NULL,
-  `pass` varchar(11) NOT NULL
+  `pass` varchar(11) NOT NULL,
+  `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`nombre`, `id`, `correo`, `pass`) VALUES
-('paco', 1, 'paco@gmail.com', 'paco');
+INSERT INTO `usuarios` (`nombre`, `id`, `correo`, `pass`, `tipo`) VALUES
+('paco', 1, 'paco@gmail.com', 'paco', 0);
 
 --
 -- Índices para tablas volcadas
