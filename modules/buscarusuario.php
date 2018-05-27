03e1e782886717a15066a55b92adf9da1b990953
Jaime Frías <jaimefrias_76@hotmail.com>
2018-05-27 23:11:19 +0200

    Añadidos resultados de buscar usuarios con AJAX

---
+++ modules/buscarusuario.php
@@ -0,0 +1,30 @@
+
+
+<?php
+include("conexion.php");
+$q = $_GET['q'];
+echo $q."<br>";
+
+$res = mysqli_query ($conexion, "SELECT * FROM usuarios WHERE nombre LIKE '%".$q."%'");
+echo "<hr>";
+if ($res->num_rows > 0) {
+    while($a_res =  mysqli_fetch_assoc($res)) {
+        echo "<form id='actualizarRol' method='post' action='panel.php#roles'>";
+        echo "<input type='hidden' name='id_usuario' value='".$a_res['id']."'>";
+        echo "<div class='resultado_usuarios'><table><tr>";
+            echo "<td>".$a_res['nombre']."</td><td class='email'>".$a_res['email']."</td><td>";
+            echo "<div class='roles'><select name='tipo'>
+                <option value='1'><i class='fa fa-user'></i> Normal</option>
+                <option value='2'>Moderador</option>
+                <option value='3'>Gestor</option>
+                <option value='4'>Superusuario</option>
+            </select></div></td><td>";
+        echo "<input type='submit' value='Actualizar' name='actualizarRol'>";
+        echo "</td></tr></table></div>";
+        echo "</form>";
+    }
+} else {
+    echo "<p class='error'><i class='fa fa-times'></i> No se han encontrado usuarios con ese nombre. </p>";
+}
+
+?>
--- modules/roles.php
+++ modules/roles.php
@@ -1,12 +1,44 @@
 <div id="roles" class="conf_roles">
     <h2><i class="fa fa-male"></i> Configurar roles </h2>
+
+    <script>
+    function buscarUsuario(str) {
+        console.log("STR:" + str);
+        // Si no se escribe nada no se muestra nada de resultado.
+        if (str == "") {
+            document.getElementById("muestra_usuarios").innerHTML = "";
+            return;
+        } else {
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("muestra_usuarios").innerHTML = this.responseText;
+                }
+            };
+            // Se envia con GET la cadena a buscar en la BD
+            xmlhttp.open("GET","modules/buscarusuario.php?q=" + str, true);
+            // Se muestra
+            xmlhttp.send();
+        }
+    }
+    </script>
     
     <form class="buscar" method="post" action="panel.php#roles">
-        <input type="text" name="usuario" placeholder="Usuario">
+        <input type="text" name="usuario" oninput="buscarUsuario(this.value)" placeholder="Usuario">
         <input type="submit" name="submit_usuario" value="Buscar">
     </form>
 
-    <?php if( isset($_POST['submit_usuario'])) {
+    <!-- El espacio donde se mostrara el resultado -->
+    <div id="muestra_usuarios"></div>
+
+    <?php 
+    if( isset($_POST['submit_usuario'])) {
         $this->buscarUsuarios($_POST['usuario']);
     }
 
