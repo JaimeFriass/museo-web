a4e0e19ed26a5d35f880f60080d08bbe43706c77
Unknown <nazaret.dragonball@gmail.com>
2018-05-30 15:24:53 +0200

    Ahora borra

--- modules/eliminar_obra.php
+++ modules/eliminar_obra.php
@@ -1,1 +1,14 @@
-ojhadfoih
+
+<?php
+include("conexion.php");
+
+$id = $_GET['id'];
+
+$res = mysqli_query( $conexion, "DELETE FROM obras WHERE id = ".$id);
+if($res == true)
+    header("Location: ../panel.php#obras"); //se va por riles
+else
+    echo "Error eliminado ";
+
+
+?>
